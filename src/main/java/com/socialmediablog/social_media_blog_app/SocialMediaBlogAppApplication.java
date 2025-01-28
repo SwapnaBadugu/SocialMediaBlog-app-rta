e05bbffd5d069c9933e145df1a469e911a4cad89
SwapnaBadugu <101745274+SwapnaBadugu@users.noreply.github.com>
2025-01-28 15:17:53 -0500

    Commit1

--- src/main/java/com/socialmediablog/social_media_blog_app/SocialMediaBlogAppApplication.java
+++ src/main/java/com/socialmediablog/social_media_blog_app/SocialMediaBlogAppApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SocialMediaBlogAppApplication {
 
-	public static void main(String[] args) {
+	public static void git status
+	main(String[] args) {
 		SpringApplication.run(SocialMediaBlogAppApplication.class, args);
 	}
 
